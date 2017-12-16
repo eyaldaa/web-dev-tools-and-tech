2b5155f3aa0f605baa9d4e854bcf15818eb90ed5
Gil Tayar <gil@tayar.org>
2017-12-16 08:38:08 +0200

    cf: recreated using create-react-app

--- packages/currency-frontend/test/unit/currency-frontend.test.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const {describe, it} = require('mocha')
-const {expect} = require('chai')
-
-describe('currency-frontend', function() {
-  it('should show hello world', async () => {
-    expect(4).to.equal(2)
-  })
-})
