7346f5636086a8d1d54f95591a3106b2202e7cdc
Gil Tayar <gil@tayar.org>
2018-01-28 06:23:38 +0200

    renamed folder

--- snippets/testing/javascript-unit-testing/test/test-calculate.js
+++ snippets/testing/javascript-unit-testing/test/test-calculate.js
@@ -5,31 +5,31 @@ const {expect} = require('chai')
 const calculatePolish = require('../src/calculate')
 
 describe('calculatePolish', function() {
-  it('should return undefined on an empty stack', async () => {
+  it('should return undefined on an empty stack', () => {
     expect(calculatePolish([])).to.be.undefined
   })
 
-  it('should return TOS if only numbers ', async () => {
+  it('should return TOS if only numbers ', () => {
     expect(calculatePolish([3, 4, 5])).to.equal(5)
   })
 
-  it('should return sum calculation', async () => {
+  it('should return sum calculation', () => {
     expect(calculatePolish([3, 4, '+'])).to.equal(7)
   })
 
-  it('should return diff calculation', async () => {
+  it('should return diff calculation', () => {
     expect(calculatePolish([3, 4, '-'])).to.equal(-1)
   })
 
-  it('should return mult calculation', async () => {
+  it('should return mult calculation', () => {
     expect(calculatePolish([3, 4, '*'])).to.equal(12)
   })
 
-  it('should return div calculation', async () => {
+  it('should return div calculation', () => {
     expect(calculatePolish([10, 5, '/'])).to.equal(2)
   })
 
-  it('should return multiple calculations', async () => {
+  it('should return multiple calculations', () => {
     expect(calculatePolish([11, 5, 8, 4,  '/', '*', '+'])).to.equal(21)
   })
 })
