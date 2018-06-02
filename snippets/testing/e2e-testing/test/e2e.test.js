4b54efd087d5270adf2ed2398cfb52533bf8a133
Gil Tayar <gil@tayar.org>
2018-06-02 19:01:31 +0300

    oops, removed some console.logs

--- snippets/testing/e2e-testing/test/e2e.test.js
+++ snippets/testing/e2e-testing/test/e2e.test.js
@@ -22,36 +22,26 @@ describe('sample web app', function() {
   it('happy path', async () => {
     await driver.get(`http://todomvc.com/examples/react/#/`)
 
-    console.log(`@@@GIL 1`)
     await driver.wait(until.elementLocated(By.css('h1')))
 
-    console.log(`@@@GIL 1`)
     expect(await (await driver.findElement(By.css('h1'))).getText()).to.equal('todos')
 
-    console.log(`@@@GIL 1`)
     const addElement = await driver.findElement(By.css('.new-todo'))
 
-    console.log(`@@@GIL 1`)
     await addElement.sendKeys('Cook!', Key.ENTER)
     await addElement.sendKeys('Clean!', Key.ENTER)
 
-    console.log(`@@@GIL 1`)
     await driver.wait(until.elementLocated(By.css('.todo-list :nth-child(2) label')))
 
-    console.log(`@@@GIL 1`)
     const secondTodo = await driver.findElement(By.css('.todo-list :nth-child(2) label'))
     expect(await secondTodo.getText()).to.equal('Clean!')
 
-    console.log(`@@@GIL 1`)
     const secondTodoCompleted = await driver.findElement(By.css('.todo-list :nth-child(2) input[type="checkbox"]'))
 
-    console.log(`@@@GIL 1`)
     await secondTodoCompleted.click()
 
-    console.log(`@@@GIL 1`)
     await (await driver.findElement(By.linkText('Completed'))).click()
 
-    console.log(`@@@GIL 1`)
     await driver.wait(until.elementLocated(By.css('.todo-list :nth-child(1) label')))
 
     const firstTodo = await driver.findElement(By.css('.todo-list :nth-child(1) label'))
